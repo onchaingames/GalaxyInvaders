c476aeb7ec50f45208546e3022a3ced6aa2a82f1
NFTmsg <132952369+onchaingames@users.noreply.github.com>
2023-07-28 17:07:49 -0700

    UI improvements?

--- packages/nextjs/components/universe/ShipStats.tsx
+++ packages/nextjs/components/universe/ShipStats.tsx
@@ -1,5 +1,4 @@
-// ShipStats.tsx
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Stat {
   name: string;
@@ -12,23 +11,41 @@ interface ShipStatsProps {
   stats: Stat[];
 }
 
-const ShipStats: React.FC<ShipStatsProps> = ({ stats }) => (
-  <div className="flex flex-col items-center">
-    <img src="/assets/ship3.png" alt="Ship" />
-    <h2 className="text-2xl font-bold my-4 hidden">Ship Stats</h2>
-    <table className="table-auto mt-5 border border-slate-500 border-spacing-10 space-x-4">
-      <tbody>
-        {stats.map((stat, index) => (
-          <tr key={index} className="">
-            <td className="border px-4 border-slate-700">{stat.name}</td>
-            <td className="border px-4 border-slate-700">{stat.value}</td>
-            <td className="border px-4 border-slate-700">{stat.deposit ? <button className="bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded">Deposit</button> : ''}</td>
-            <td className="border py-2 px-4 border-slate-700">{stat.withdraw ? <button className="bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded">Withdraw</button> : ''}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
+const ShipStats: React.FC<ShipStatsProps> = ({ stats }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleButtonClick = () => {
+    !showModal ? setShowModal(true) :setShowModal(false);
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      <img src="/assets/ship3.png" alt="Ship" />
+      <h2 className="text-2xl font-bold my-4 hidden">Ship Stats</h2>
+      <table className="table-auto mt-5 border border-slate-500 border-spacing-10 space-x-4">
+        <tbody>
+          {stats.map((stat, index) => (
+            <tr key={index} className="">
+              <td className="border px-4 border-slate-700">{stat.name}</td>
+              <td className="border px-4 border-slate-700">{stat.value}</td>
+              <td className="border px-4 border-slate-700">{stat.deposit ? <button className="bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded" onClick={handleButtonClick}>Deposit</button> : ''}</td>
+              <td className="border py-2 px-4 border-slate-700">{stat.withdraw ? <button className="bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded" onClick={handleButtonClick}>Withdraw</button> : ''}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+<dialog id="my_modal" className={`modal ${showModal ? 'modal-open' : ''}`}>
+  <form method="dialog" className="modal-box">
+    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={handleButtonClick}>✕</button>
+    <h3 className="font-bold text-lg">Hello!</h3>
+    <p className="py-4">Press ESC key or click on ✕ button to close</p>
+  </form>
+</dialog>
+
+  
+    </div>
+  );
+};
 
 export default ShipStats;
--- packages/nextjs/pages/galaxy.tsx
+++ packages/nextjs/pages/galaxy.tsx
@@ -16,19 +16,18 @@ const stats = [
 const galaxy: NextPage = () => {
   const [selectedPlanet, setSelectedPlanet] = useState(null);
   const [selectedTarget, setSelectedTarget] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
   const handlePlanetClick = (planet: SetStateAction<null>) => {
     setSelectedPlanet(planet);
-    console.log(selectedPlanet); // Add this line
-  };
-
-  const handleBackClick = () => {
-    setSelectedPlanet(null);
-    setSelectedTarget(null); // Clear the selected target when the "Back" button is clicked
   };
 
   const handleTargetSelect = (target: any) => {
     setSelectedTarget(target);
+  };
+
+  const handleDepositWithdrawClick = () => {
+    setShowModal(true);
   };
 
   return (
@@ -37,36 +36,16 @@ const galaxy: NextPage = () => {
         title="Planet Victory"
         description="Battle for yield and Gov tokens"
       >
-        {/* We are importing the font this way to lighten the size of SE2. */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
       </MetaHeader>
-      {selectedPlanet ? (
-        <div className="planets mt-10 mb-4 bg-black grid grid-cols-3 items-baseline place-content-center gap-10">
-          
-          <div className="planetTitle col-start-2 text-center ">
-            <h2>{selectedPlanet.name}</h2>
-            <p>{selectedPlanet.desc}</p>
-          </div>
-          <div className="backButton col-start-1 justify-self-end">
-            <button 
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              onClick={handleBackClick}
-              >
-              Back
-            </button>
-          </div>
-          <PlanetSurface planet={selectedPlanet} onBackClick={handleBackClick} onTargetSelect={handleTargetSelect} />
-          <TargetingSystem target={selectedTarget} />
-        </div>
-      ) : (
-        <div className="planetGrid mb-40 mt-20 ">
-          <PlanetGrid onPlanetClick={handlePlanetClick} />
-        </div>
-      )}
-      <div className="shipStats mb-40 ">
-        <ShipStats stats={stats} />
+      <div className="flex flex-col items-center">
+        <PlanetSurface planet={selectedPlanet} onTargetSelect={handleTargetSelect} />
+        <ShipStats stats={stats}  />
+        <TargetingSystem target={selectedTarget} />
+        <PlanetGrid onPlanetClick={handlePlanetClick} />
       </div>
+      {showModal && <DepositWithdrawModal onClose={() => setShowModal(false)} />}
     </>
   );
 };
