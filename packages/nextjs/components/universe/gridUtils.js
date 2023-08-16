889569fe1081eddff1c18d95cf6e15b3d8379909
NFTmsg <132952369+onchaingames@users.noreply.github.com>
2023-08-16 16:57:45 -0700

    adding file

---
+++ packages/nextjs/components/universe/gridUtils.js
@@ -0,0 +1,38 @@
+// gridUtils.js
+
+const homeworlds = ['Lido', 'Rocket', 'Frax', 'Invader L1', 'Invader L2', 'Invader L3'];
+
+export const generateGrid = () => {
+  const grid = [];
+  for (let i = 0; i < 16; i++) {
+    const row = [];
+    for (let j = 0; j < 16; j++) {
+      const rand = Math.random();
+      if (rand < 0.3) {
+        row.push({
+          type: 'rocket',
+          homeworld: homeworlds[Math.floor(Math.random() * homeworlds.length)],
+          ammo: Math.floor(Math.random() * 100),
+          experience: Math.floor(Math.random() * 1000),
+          victoryPoints: Math.floor(Math.random() * 5000),
+        });
+      } else if (rand < 0.35) {
+        row.push({
+          type: 'alien',
+          ammoCost: Math.floor(Math.random() * 10),
+          victoryPoints: Math.floor(Math.random() * 100),
+          age: Math.floor(Math.random() * 10),
+        });
+      } else {
+        row.push({
+          type: 'blank',
+          ammoCost: Math.floor(Math.random() * 10),
+          victoryPoints: Math.floor(Math.random() * 100),
+          age: Math.floor(Math.random() * 10),
+        });
+      }
+    }
+    grid.push(row);
+  }
+  return grid;
+};
