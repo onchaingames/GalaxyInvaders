2fd0bf24a348ca44d2015b213b907f48fc6d4418
NFTmsg <132952369+onchaingames@users.noreply.github.com>
2023-08-23 10:46:53 -0700

    improving copy

--- packages/nextjs/pages/galaxy.tsx
+++ packages/nextjs/pages/galaxy.tsx
@@ -6,10 +6,7 @@ import PlanetSurface from '~~/components/universe/PlanetSurface';
 import ShipStats from '~~/components/universe/ShipStats';
 import TargetingSystem from '~~/components/universe/TargetingSystem';
 
-
-
 const galaxy: NextPage = () => {
-
   const homeworlds = ['Lido', 'Rocket', 'Frax'];
 
   const planetsData = [
@@ -20,7 +17,6 @@ const galaxy: NextPage = () => {
 
   const [planets, setPlanets] = useState(planetsData);
   const [selectedPlanetIndex, setSelectedPlanetIndex] = useState(0);
-
   const [selectedTargetIndex, setSelectedTargetIndex] = useState(0);
 
   return (
@@ -33,15 +29,11 @@ const galaxy: NextPage = () => {
         <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
       </MetaHeader>
 
-      <div className="planets mt-10 mb-4 bg-black" style={{
-        backgroundImage: `
-        url("data:image/svg+xml,%3Csvg width='300' height='300' xmlns='http://www.w3.org/2000/svg'%3E%3Cdefs%3E%3Cpattern id='starry-pattern' x='0' y='0' width='1' height='1' viewBox='0 0 300 300' fill='%23ffffff'%3E%3Ccircle cx='30' cy='30' r='1' /%3E%3Ccircle cx='150' cy='150' r='2' /%3E%3Ccircle cx='270' cy='30' r='1' /%3E%3Ccircle cx='30' cy='270' r='1' /%3E%3Ccircle cx='270' cy='270' r='1' /%3E%3Ccircle cx='90' cy='120' r='0.5' /%3E%3Ccircle cx='210' cy='60' r='0.5' /%3E%3Ccircle cx='60' cy='210' r='0.5' /%3E%3Ccircle cx='240' cy='180' r='0.5' /%3E%3Ccircle cx='120' cy='240' r='1.5' /%3E%3Ccircle cx='180' cy='90' r='0.7' /%3E%3Ccircle cx='45' cy='165' r='0.6' /%3E%3Ccircle cx='255' cy='105' r='0.8' /%3E%3Ccircle cx='75' cy='75' r='0.9' /%3E%3Ccircle cx='225' cy='225' r='0.4' /%3E%3Ccircle cx='105' cy='45' r='1.2' /%3E%3Ccircle cx='195' cy='255' r='0.3' /%3E%3Ccircle cx='135' cy='195' r='0.6' /%3E%3Ccircle cx='165' cy='15' r='0.7' /%3E%3C/pattern%3E%3C/defs%3E%3Crect width='300' height='300' fill='url(%23starry-pattern)' /%3E%3C/svg%3E")
-        `
-      }}>
+      <div className="planets mt-10 mb-4 bg-black" style={{ backgroundImage: `...` }}>
         <div className="planetTitle mb-4 text-center text-white text-2xl">
-          <h2 className = "text-title">Planet {planets[selectedPlanetIndex].name}</h2>
+          <h2 className="text-title">Planet {planets[selectedPlanetIndex].name}</h2>
         </div>
-        <div className="flex justify-center items-stretch gap-10 w-full">
+        <div className="flex justify-center items-stretch gap-10 w-full overflow-x-auto">
           <div className="flex flex-1 justify-end">
             <div className="bg-gray-800 rounded shipStats flex-shrink-0 min-w-[275px]">
               <ShipStats />
@@ -52,15 +44,15 @@ const galaxy: NextPage = () => {
               planets={planets} 
               selectedPlanetIndex={selectedPlanetIndex} 
               selectedTargetIndex={selectedTargetIndex}
-              setSelectedTargetIndex = {setSelectedTargetIndex}
+              setSelectedTargetIndex={setSelectedTargetIndex}
             />
           </div>
           <div className="flex flex-1 justify-start">
-            <div className="bg-gray-800 rounded targeting-system flex-shrink-0 min-w-[275px]"> {/* min-w-[233px] sets the minimum width */}
+            <div className="bg-gray-800 rounded targeting-system flex-shrink-0 min-w-[275px]">
               {selectedTargetIndex && <TargetingSystem 
-              planets={planets} 
-              selectedPlanetIndex={selectedPlanetIndex} 
-              selectedTargetIndex={selectedTargetIndex}
+                planets={planets} 
+                selectedPlanetIndex={selectedPlanetIndex} 
+                selectedTargetIndex={selectedTargetIndex}
               />}
             </div>
           </div>
@@ -72,10 +64,10 @@ const galaxy: NextPage = () => {
 
       <div className="mt-4">
         <PlanetGrid 
-        planets = {planets} 
-        setPlanets = {setPlanets}
-        setSelectedPlanetIndex = {setSelectedPlanetIndex} 
-        selectedPlanetIndex = {selectedPlanetIndex} 
+          planets={planets} 
+          setPlanets={setPlanets}
+          setSelectedPlanetIndex={setSelectedPlanetIndex} 
+          selectedPlanetIndex={selectedPlanetIndex} 
         />
       </div>
     </>
--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
           </p>
           <ul className="list-disc mt-10">
             <li className="mb-2 font-bold">💰 Stake stEth to Mint a spaceship NFT and earn boosted yield up to <span className = "text-purple-500">35%</span> in {UTILITY_TOKEN.emoji}{UTILITY_TOKEN.symbol}</li> 
-            <li className="mb-2 font-bold">🔫 Earn Exp, Battle other ships for yield by firing {UTILITY_TOKEN.emoji}{UTILITY_TOKEN.symbol} Tokens entirely on chain</li>
+            <li className="mb-2 font-bold">🚀 Battle other ships for yield by firing {UTILITY_TOKEN.emoji}{UTILITY_TOKEN.symbol} Tokens entirely on chain</li>
             <li className="mb-2 font-bold">👾 Destroy Aliens and earn {GOV_TOKEN.emoji}{GOV_TOKEN.title} Gov Tokens which can be staked to earn fees</li>
           </ul>
         </div>
@@ -32,28 +32,30 @@ const Home: NextPage = () => {
         </div>
         <div className="mt-10 max-w-[800px] px-8 w-full">
             <h2 className="text-title text-3xl mb-4 text-center font-bold">Rules of the Galaxy</h2>
-            <h3 className="text-xl font-bold mb-2 text-title">STAKING</h3>
+            <h3 className="text-xl font-bold mb-2 text-title">💰STAKING</h3>
             <ul className="list-disc mb-4">
                 <li>Mint a Space Ship on a homeworld by choosing a staking service and depositing Eth.</li>
                 <li>As your staked Eth generates rewards, they are wrapped into Ammo Tokens.</li>
+                <li>Your Yield can be higher or lower depending on gameplay events, but your stake is never at risk</li>
                 <li>These Ammo tokens can be used for gameplay, withdrawn, or unwrapped back to Eth.</li>
                 <li>You can travel to other planet homeworlds, but beware foreign fighters who may attack you!</li>
             </ul>
-            <h3 className="text-xl font-bold mb-2 text-title">ALIEN BATTLE</h3>
+            <h3 className="text-xl font-bold mb-2 text-title">{GOV_TOKEN.emoji}ALIEN BATTLE</h3>
             <ul className="list-disc mb-4">
-                <li>Aliens are NPCs that randomly attack the planets. They try and steal ammo.</li>
-                <li>Under their stats is how much Ammo they require to be destroyed as well as how much Exp and Gov Tokens you will win for defeating them.</li>
-                <li>They also have a time limit before they respawn elsewhere. Gaining Exp helps you attack and defend attacks from foreign ships.</li>
+                <li>Aliens are NPCs that randomly attack the planets. They may try and steal ammo.</li>
+                <li>They differ in their reward value and the Amount of Ammo required to defeat them.</li>
+                <li>Your Exp earned is equal to the amount of Ammo you fired to successfully defeat an alien.</li>
+                <li>Each alien has a lifecyle before they respawn elsewhere. </li>
             </ul>
-            <h3 className="text-xl font-bold mb-2 text-title">SHIP BATTLE</h3>
+            <h3 className="text-xl font-bold mb-2 text-title">🚀SHIP BATTLE</h3>
             <ul className="list-disc">
                 <li>You may attack or be attacked by foreign ships.</li>
                 <li>To attack another ship you fire Ammo at them. If you are successful, you win double the amount that you fired from your target.</li>
                 <li>If you miss, the ammo is distributed out to all stakers on the planet.</li>
-                <li>Your odds of hitting are based on your Exp level relative to theirs.</li>
+                <li>Your odds of winning the battle are based on your ships relative Exp level.</li>
             </ul>
             <h2 className="text-2xl mt-10 text-center font-bold text-title">Galaxy Tokenomics</h2>
-            <h3 className="text-xl text-title font-bold mb-2">AMMO TOKEN</h3>
+            <h3 className="text-xl text-title font-bold mb-2">{UTILITY_TOKEN.emoji}AMMO TOKEN</h3>
             <ul className="list-disc mb-4">
                 <li>Ammo tokens are utility tokens.</li>
                 <li>Ammo tokens represents your staked ETH's yield and can be redeemed for their underlying ETH at anytime. </li>
@@ -64,11 +66,12 @@ const Home: NextPage = () => {
                   in a LP pool or staking in a validator service such as Rocket Pool.
                 </li>
             </ul>
-            <h3 className="text-xl font-bold mb-2 text-title">GALAXY TOKEN</h3>
+            <h3 className="text-xl font-bold mb-2 text-title">{GOV_TOKEN.emoji}GALAXY TOKEN</h3>
             <ul className="list-disc mb-4">
                 <li>Galaxy tokens are the governance tokens for the {TITLE} game.</li>
                 <li>Earn Galaxy Tokens by destroying Aliens.</li>
                 <li>Galaxy Tokens will control the DAO and earn protocol fees</li>
+                <li>Emissions rate of Galaxy tokens will be scheduled to decrease over time with a hard cap, subject to DAO votes.</li>
             </ul>
             <h3 className="text-xl font-bold mb-2 text-title">TOKEN ALLOCATION</h3>
             Galaxy Token Distribution:
@@ -80,16 +83,24 @@ const Home: NextPage = () => {
               </ul>
             <ul className="mb-2">
                 <li>The treasury funds will be used for activities like funding audits, marketing, rewarding community ect.</li>
-                <li>Emissions rate of Galaxy tokens will be scheduled to decrease over time with a hard cap, subject to DAO votes.</li>
             </ul>
             <h3 className="text-xl font-bold mb-2 text-title">SHIP NFT</h3>
-        <img src="/assets/ship3.png" alt="Ship" />
+        <img className="text-center self-center items-center" src="/assets/ship3.png" alt="Ship" />
             <ul className="list-disc mb-2">
-                <li>Your ship NFT utilizes a new type of smart contract standard (EIP-6551) aka <Link href="https://tokenbound.org/" className="text-link">Token Bound NFTs</Link> that allows the NFT itself to act as a smart wallet and OWN
-                  other tokens. This new functionality is leveraged to allow your ship to own its staked ETH, AMMO and GALAXY TOKENS and to interact with the smart contract on your behalf.
-                  Beware this means that if you sell your ship, you sell all your game inventory and charactieristics. As owner of your ship you can always transfer tokens between your 
-                  ship NFT and your wallet. However traits like Exp remain attributed to your wallet permanently.
+                <li>Your ship NFT utilizes a new type of smart contract standard (EIP-6551) aka <Link href="https://tokenbound.org/" className="text-link">Token Bound NFTs </Link>  
+                that allows the NFT itself to act as a smart wallet and OWN other tokens.</li>
+                <li> This new functionality is leveraged to allow your ship to own its staked ETH, AMMO and GALAXY TOKENS and to interact with the smart contract on your behalf.</li>
+                <li>Beware this means that if you sell your ship, you sell all your game inventory and characteristics.</li>
+                <li> As owner of your ship you can always transfer tokens between your ship NFT and your wallet. However traits like Exp remain attributed to your wallet permanently.
                 </li>
+            </ul>
+            <h2 className="text-2xl mt-10 text-center font-bold text-red-600">Risks</h2>
+            <ul className="list-disc mb-4">
+                <li className = "text-red-600">This Game is currently unaudited, you may lose what you put in. Once funds are available an audit will be completed.</li>
+                <li>Barring a smart contract bug, your Eth stake and {GOV_TOKEN.symbol}s should be safe, but your ships Ammo is always at risk.</li>
+                <li>If you sell your ship, all your coins and Exp will be transferred.</li>
+                <li>If you buy a ship on a public market beware that the tokens held in the ship may be withdrawn right before the transfer is complete. 
+                  The Exp points will remain associated to the ship. Markets are currently working on a solution to this new issue introduced by the new Token Bound account contracts.</li>
             </ul>
         </div>
       </div>
