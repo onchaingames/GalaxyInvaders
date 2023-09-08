2d8b0f900412dd66815b2c0616c6665dace68dfe
NFTmsg <132952369+onchaingames@users.noreply.github.com>
2023-09-07 17:23:30 -0700

    adding pages

---
+++ packages/nextjs/pages/spaceshipLab.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from "next";
+import { MetaHeader } from "~~/components/MetaHeader";
+import { ContractData } from "~~/components/example-ui/ContractData";
+import { Mint } from "~~/components/example-ui/Mint";
+import SvgBuilder from "~~/components/universe/SvgBuilder";
+
+const spaceshipLab: NextPage = () => {
+  return (
+    <>
+      <MetaHeader
+        title="Example UI | Scaffold-ETH 2"
+        description="Example UI created with 🏗 Scaffold-ETH 2, showcasing some of its features."
+      >
+        {/* We are importing the font this way to lighten the size of SE2. */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
+      </MetaHeader>
+        <SvgBuilder />
+    </>
+  );
+};
+
+export default spaceshipLab;
